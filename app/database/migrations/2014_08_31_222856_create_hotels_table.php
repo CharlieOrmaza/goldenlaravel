dfd33c8ea5827f41c43474f6723c605a50d6dffa
pampa2 <pampa132639@gmail.com>
2014-09-09 11:53:43 -0500

    tablaHoteles
    hoteles

--- app/database/migrations/2014_08_31_222856_create_hotels_table.php
+++ app/database/migrations/2014_08_31_222856_create_hotels_table.php
@@ -15,7 +15,21 @@ class CreateHotelsTable extends Migration {
 		Schema::create('hotels', function(Blueprint $table)
 		{
 			$table->increments('id');
-
+            $table->string('noPapeleta');
+			$table->string('nombrePax');
+			$table->string('telefono');
+			$table->string('email');
+			$table->string('referencia');
+            $table->string('destino');
+			$table->string('operador');
+			$table->string('nombreHotel');
+			$table->date('fechaDeEntrada');
+			$table->date('fechaDeSalida');
+			$table->string('sgl');
+			$table->string('dbl');
+			$table->string('tpl');
+			$table->string('cpl');
+			$table->string('otros');
 			$table->timestamps();
 		});
 	}
